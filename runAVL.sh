c1203b3d308a42402a0ac6a0e4babbab1ba82360
saransh738 <74806993+saransh738@users.noreply.github.com>
2021-08-15 02:15:38 +0530

    Create runAVL.sh

---
+++ runAVL.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "Provide input on console"
+    java DriverAVL
+fi
+if [ $# -eq 1 ]; then
+    java DriverAVL < "${1}"
+fi
+if [ $# -eq 2 ]; then
+    java DriverAVL < "${1}" > "${2}"
+fi
+© 2021 GitHub, Inc.
